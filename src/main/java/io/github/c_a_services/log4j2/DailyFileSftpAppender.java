66cb9cc595382efe8af84f2a22a38820eb30ae28
cas <cas@adr.de>
2018-11-13 15:00:09 +0100

    try with sshj
    ERROR StatusLogger DailyFileSftpAppender:Error
     net.schmizz.sshj.userauth.UserAuthException: Exhausted available authentication methods
    	at net.schmizz.sshj.SSHClient.auth(SSHClient.java:231)
    	at net.schmizz.sshj.SSHClient.authPublickey(SSHClient.java:346)
    	at net.schmizz.sshj.SSHClient.authPublickey(SSHClient.java:395)
    	at net.schmizz.sshj.SSHClient.authPublickey(SSHClient.java:325)
    	at io.github.c_a_services.log4j2.DailyFileSftpAppender.append(DailyFileSftpAppender.java:117)

--- src/main/java/io/github/c_a_services/log4j2/DailyFileSftpAppender.java
+++ src/main/java/io/github/c_a_services/log4j2/DailyFileSftpAppender.java
@@ -1,9 +1,16 @@
 package io.github.c_a_services.log4j2;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.security.PublicKey;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.logging.log4j.core.Filter;
 import org.apache.logging.log4j.core.Layout;
@@ -14,6 +21,13 @@ import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
 import org.apache.logging.log4j.core.config.plugins.PluginElement;
 import org.apache.logging.log4j.core.config.plugins.PluginFactory;
 import org.apache.logging.log4j.core.layout.PatternLayout;
+import org.apache.logging.log4j.status.StatusLogger;
+
+import net.schmizz.sshj.SSHClient;
+import net.schmizz.sshj.sftp.OpenMode;
+import net.schmizz.sshj.sftp.RemoteFile;
+import net.schmizz.sshj.transport.verification.HostKeyVerifier;
+import net.schmizz.sshj.userauth.keyprovider.KeyProvider;
 
 /**
  *
@@ -21,21 +35,31 @@ import org.apache.logging.log4j.core.layout.PatternLayout;
 @Plugin(name = "DailyFileSftpAppender", category = "Core", elementType = "appender", printObject = true)
 public class DailyFileSftpAppender extends AbstractAppender {
 
+	private static final StatusLogger LOGGER = StatusLogger.getLogger();
+
+	private String userName;
 	private String privateKeyResource;
-	private String privateKey;
+	private String passPhrase;
 	private String filePattern;
 	private String hostName;
+	private String pathName;
+
+	private String publicKeyResource;
 
 	/**
+	 * @param aPublicKeyResource TODO
 	 *
 	 */
-	protected DailyFileSftpAppender(String aName, Filter aFilter, Layout<? extends Serializable> aLayout, boolean ignoreExceptions, String aPrivateKeyResource,
-			String aFilePattern, String aHostName, String aPrivateKey) {
+	protected DailyFileSftpAppender(String aName, Filter aFilter, Layout<? extends Serializable> aLayout, boolean ignoreExceptions, String aUserName,
+			String aPublicKeyResource, String aPrivateKeyResource, String aFilePattern, String aHostName, String aPathName, String aPassPhrase) {
 		super(aName, aFilter, aLayout, ignoreExceptions);
+		userName = aUserName;
+		publicKeyResource = aPublicKeyResource;
 		privateKeyResource = aPrivateKeyResource;
 		filePattern = aFilePattern;
 		hostName = aHostName;
-		privateKey = aPrivateKey;
+		pathName = aPathName;
+		passPhrase = aPassPhrase;
 
 	}
 
@@ -43,10 +67,13 @@ public class DailyFileSftpAppender extends AbstractAppender {
 	public static DailyFileSftpAppender createAppender(@PluginAttribute("name") String name, @PluginAttribute("ignoreExceptions") boolean ignoreExceptions,
 			@PluginElement("Layout") Layout<? extends Serializable> aLayout, //
 			@PluginElement("Filters") Filter filter, //
+			@PluginAttribute("userName") String aUserName, //
+			@PluginAttribute("publicKeyResource") String aPublicKeyResource, //
 			@PluginAttribute("privateKeyResource") String aPrivateKeyResource, //
-			@PluginAttribute("privateKey") String aKeyResource, //
+			@PluginAttribute("passPhrase") String aPassPhrase, //
 			@PluginAttribute("filePattern") String aFilePattern, //
-			@PluginAttribute("hostName") String aHostName) {
+			@PluginAttribute("hostName") String aHostName, //
+			@PluginAttribute("pathName") String aPathName) {
 
 		if (name == null) {
 			LOGGER.error("No name provided for StubAppender");
@@ -59,7 +86,8 @@ public class DailyFileSftpAppender extends AbstractAppender {
 		} else {
 			tempLayout = aLayout;
 		}
-		return new DailyFileSftpAppender(name, filter, tempLayout, ignoreExceptions, aPrivateKeyResource, aFilePattern, aHostName, aKeyResource);
+		return new DailyFileSftpAppender(name, filter, tempLayout, ignoreExceptions, aUserName, aPublicKeyResource, aPrivateKeyResource, aFilePattern,
+				aHostName, aPathName, aPassPhrase);
 	}
 
 	/**
@@ -69,37 +97,104 @@ public class DailyFileSftpAppender extends AbstractAppender {
 	public void append(LogEvent aEvent) {
 		Serializable tempString = getLayout().toSerializable(aEvent);
 		LocalDate tempLocalDateTime = Instant.ofEpochMilli(aEvent.getTimeMillis()).atZone(ZoneId.systemDefault()).toLocalDate();
-		String tempFileName = tempLocalDateTime + "-" + getFilePattern();
-		System.out.println("tempFileName=" + tempFileName);
-		System.out.println("tempString=" + tempString);
+		String tempFileName = tempLocalDateTime + "-" + filePattern;
+
+		SSHClient ssh = new SSHClient();
+		try {
+			//			ssh.loadKnownHosts();
+			ssh.addHostKeyVerifier(new HostKeyVerifier() {
+
+				@Override
+				public boolean verify(String aHostname, @SuppressWarnings("unused") int aPort, @SuppressWarnings("unused") PublicKey aKey) {
+					return aHostname.equals(hostName);
+				}
+			});
+			ssh.connect(hostName);
+			try {
+				KeyProvider tempKey = loadKey(ssh);
+
+				ssh.authPublickey(userName, tempKey);
+
+				Set<OpenMode> tempOpenModes = new HashSet<>();
+				tempOpenModes.add(OpenMode.WRITE);
+				tempOpenModes.add(OpenMode.APPEND);
+				RemoteFile tempFile = ssh.newSFTPClient().open(pathName + tempFileName, tempOpenModes);
+			} finally {
+				ssh.disconnect();
+			}
+		} catch (IOException | RuntimeException e) {
+			logError("Error", e);
+			throw new RuntimeException(e);
+		}
+
 	}
 
 	/**
-	 * @see #filePattern
+	 * @param aSsh
+	 * @throws IOException
+	 *
 	 */
-	public String getFilePattern() {
-		return filePattern;
+	private KeyProvider loadKey(SSHClient aSsh) throws IOException {
+		File tempTempFile = createTempKeyFile(privateKeyResource, null);
+		String tempPublicKeyExtension = publicKeyResource.substring(privateKeyResource.length());
+		// see net.schmizz.sshj.userauth.keyprovider.KeyProviderUtil.detectKeyFileFormat(File)
+		File tempTempPub = createTempKeyFile(publicKeyResource, new File(tempTempFile.getAbsolutePath() + tempPublicKeyExtension));
+		if (LOGGER.isDebugEnabled()) {
+			logDebug("tempTempPub=" + tempTempPub + " for " + publicKeyResource);
+		}
+		KeyProvider tempKey;
+		if (passPhrase == null) {
+			tempKey = aSsh.loadKeys(tempTempFile.getAbsolutePath());
+		} else {
+			tempKey = aSsh.loadKeys(tempTempFile.getAbsolutePath(), passPhrase);
+		}
+		//	TODO	tempTempFile.delete();
+		//	TODO	tempTempPub.delete();
+		if (LOGGER.isDebugEnabled()) {
+			logDebug("Loaded " + tempKey + " for " + privateKeyResource);
+		}
+		return tempKey;
 	}
 
 	/**
-	 * @see #privateKeyResource
+	 *
 	 */
-	public String getPrivateKeyResource() {
-		return privateKeyResource;
+	private void logDebug(String aString) {
+		LOGGER.debug("DailyFileSftpAppender:" + aString);
 	}
 
 	/**
-	 * @see #privateKey
+	 * @param aResource
+	 * @throws IOException
+	 *
 	 */
-	public String getPrivateKey() {
-		return privateKey;
+	private File createTempKeyFile(String aResource, File aTargetFile) throws IOException {
+		File tempFile = aTargetFile;
+		if (tempFile == null) {
+			tempFile = File.createTempFile("DailyFileSftpAppender", aResource.replace('/', '_'));
+		}
+		tempFile.deleteOnExit();
+		InputStream tempIn = Thread.currentThread().getContextClassLoader().getResourceAsStream(aResource);
+		if (tempIn == null) {
+			throw new IllegalArgumentException(privateKeyResource + " not found.");
+		}
+		try (FileOutputStream tempOut = new FileOutputStream(tempFile)) {
+			byte[] tempBuff = new byte[1024 * 16];
+			int tempRead = tempIn.read(tempBuff);
+			while (tempRead > 0) {
+				tempOut.write(tempBuff, 0, tempRead);
+				tempRead = tempIn.read(tempBuff);
+			}
+			tempIn.close();
+		}
+		return tempFile;
 	}
 
 	/**
-	 * @see #hostName
+	 *
 	 */
-	public String getHostName() {
-		return hostName;
+	private void logError(String aString, Exception aE) {
+		LOGGER.error("DailyFileSftpAppender:" + aString, aE);
 	}
 
 }
